8cf85edd2bf3debd208605a8937c050c85069212
Olena Voina <105532756+olli20@users.noreply.github.com>
2023-03-07 13:37:25 +0100

    on progress

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
 
-import {store, persistor} from './redux/store';
+import store from './redux/store';
 
 import './index.scss';
 import './styles/normalize.scss';
@@ -12,10 +11,8 @@ import App from './components/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>      
+    <Provider store={store}>         
           <App />
-      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,20 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit';
-import { persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-
 import contactsReducer from './contacts/contacts-slice';
 import filterReducer from './filter/filter-slice';
 
 const rootReducer = combineReducers({
     contacts: contactsReducer,
     filter: filterReducer,
-})
+});
 
-const persistConfig = {
-    key: 'root',
-    storage,
-}
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-export default persistedReducer;
+export default rootReducer;
